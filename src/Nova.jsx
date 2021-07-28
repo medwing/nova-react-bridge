dc3a18b614010de7497e725a9a7085b063c58141
Sirajul Muneer <sirajul.muneer@medwing.com>
2021-07-28 09:29:37 +0200

    Update Nova.jsx

--- src/Nova.jsx
+++ src/Nova.jsx
@@ -25,6 +25,7 @@ class Nova extends Component {
       const id = this.placeholder.current.getAttribute('data-hypernova-id');
       const { name } = this.props;
       const customEvent = new CustomEvent('NovaMount', { detail: { id, name } });
+      document.dispatchEvent(customEvent);
     }
   }
 
